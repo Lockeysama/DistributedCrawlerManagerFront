9cb98083e43291fccfcb995451bca215e003d749
陈奕涛 <>
2019-02-12 14:44:07 +0800

    修改文件结构

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import './plugins/ant-design-vue.js'
+import router from './router.js'
+import './plugins/element.js'
+import './plugins/echarts.js'
+import store from './plugins/vuex'
+import moment from 'vue-moment'
+
 
 Vue.config.productionTip = false
 
+Vue.use(moment)
+
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
--- src/router.js
+++ src/router.js
@@ -1,23 +1,128 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+import Login from './views/Login/Login.vue'
+import Layout from './components/Layout/Layout.vue'
+import Home from './views/Home/Home.vue'
+import Configuration from './views/Configuration/Configuration.vue'
+import ModulesCrawler from './views/Modules/ModulesCrawler.vue'
+import ModulesWSCrawler from './views/Modules/ModulesWSCrawler.vue'
+import ModulesParser from './views/Modules/ModulesParser.vue'
+import TaskTiming from './views/Task/TaskTiming.vue'
+import TaskKeep from './views/Task/TaskKeep.vue'
+import Proxies from './views/Proxies/Proxies.vue'
+import MonitorWorkers from './views/Monitor/MonitorWorkers.vue'
+import MonitorTask from './views/Monitor/MonitorTask.vue'
+import MonitorEvent from './views/Monitor/MonitorEvent.vue'
+import Wedis from './views/Wedis/Wedis.vue'
+
+const router = new Router({
   routes: [
+    {path: '/Login', component: Login },
+    {path: '/', redirect: '/Home' },
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '/Home', 
+      component: Layout, 
+      children: [
+        {path: '/Home', component: Home, meta: {requireAuth: true}}
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/Configuration', 
+      component: Layout, 
+      children: [
+        {path: '/Configuration', component: Configuration, meta: {requireAuth: true}}
+      ]
+    },
+    {
+      path: '/Modules/Crawler', 
+      component: Layout, 
+      children: [
+        {path: '/Modules/Crawler', component: ModulesCrawler, meta: {requireAuth: true}}
+      ]
+    },
+    {
+      path: '/Modules/WSCrawler', 
+      component: Layout, 
+      children: [
+        {path: '/Modules/WSCrawler', component: ModulesWSCrawler, meta: {requireAuth: true}}
+      ]
+    },
+    {
+      path: '/Modules/Parser', 
+      component: Layout, 
+      children: [
+        {path: '/Modules/Parser', component: ModulesParser, meta: {requireAuth: true}}
+      ]
+    },
+    {
+      path: '/Task/Timing', 
+      component: Layout, 
+      children: [
+        {path: '/Task/Timing', component: TaskTiming, meta: {requireAuth: true}}
+      ]
+    },{
+      path: '/Task/Keep', 
+      component: Layout, 
+      children: [
+        {path: '/Task/Keep', component: TaskKeep, meta: {requireAuth: true}}
+      ]
+    },
+    {
+      path: '/Proxies', 
+      component: Layout, 
+      children: [
+        {path: '/Proxies', component: Proxies, meta: {requireAuth: true}}
+      ]
+    },
+    {
+      path: '/Monitor/Workers', 
+      component: Layout, 
+      children: [
+        {path: '/Monitor/Workers', component: MonitorWorkers, meta: {requireAuth: true}}
+      ]
+    },
+    {
+      path: '/Monitor/Task', 
+      component: Layout, 
+      children: [
+        {path: '/Monitor/Task', component: MonitorTask, meta: {requireAuth: true}}
+      ]
+    },
+    {
+      path: '/Monitor/Event', 
+      component: Layout, 
+      children: [
+        {path: '/Monitor/Event', component: MonitorEvent, meta: {requireAuth: true}}
+      ]
+    },
+    {
+      path: '/Wedis', 
+      component: Layout, 
+      children: [
+        {path: '/Wedis', component: Wedis, meta: {requireAuth: true}}
+      ]
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requireAuth) {
+    if (router.app.$store.state.token) {
+      next();
+    }
+    else {
+      next({
+        path: '/Login',
+        query: {redirect: to.fullPath}
+      })
+    }
+  }
+  else {
+    next();
+  }
+})
+
+export default router
